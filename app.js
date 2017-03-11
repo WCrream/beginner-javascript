435b86fbd397f4891276db41474d694eaaca0937
WCrream <zhuber2471@myhta.org>
2017-03-10 14:28:04 -1000

    I finished all the things

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+console.log("Sah Dude")
 function fortuneTeller(){
+	var numOfKids = 2;
+	var partnerName = 
+	var geographicLocation = Ireland
+	var jobTitle = YouTuber
+	alert("You will be a "+ jobTitle +", living in "+ geographicLocation +", you will be married to "+ partnerName +", and you will have + numOfKids +)
 
 }
-
+function fortuneTeller
 function ageCalculator(){
 
 }
